261daf788bb19037cb7b3c93e68eea75de3c0dcf
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-07-05 16:57:27 -0700

    Implemented displaying circles on the canvas - index.js

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,34 +1,71 @@
-let sketch = p => {
-    p.DEFAULT_FRAMERATE = 60;
-    p.bg = [255, 204, 0];
-
-    p.setup = () => {
-        p.frameRate(p.DEFAULT_FRAMERATE);
-        p.createCanvas(500, 500);
-        p.background(p.bg[0], p.bg[1], p.bg[2]);
+class Position {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
-
-    p.draw = () => {
-        p.background(p.bg[0], p.bg[1], p.bg[2]);
-        p.rect(p.frameCount, 100, 50, 50);
-    }
-
-    p.changeBackground = (r, g, b) => {
-        console.log("CHANGE")
-        p.bg = [r, g, b];
-    }
-
-    p.mousePressed = () => {
-        console.log("PRESS")
-
-        if (p.bg[0] == 51) {
-            p.changeBackground(255, 204, 0);
-        } else {
-            p.changeBackground(51, 51, 255);
-        }
-    }
-
-    
 }
 
-let myCanvas = new p5(sketch);
+class AbstractPhysicsObject {
+    constructor(x, y) {
+        if (this.constructor == AbstractPhysicsObject) {
+            throw new Error("Cannot instantiate Abstract Class Objects");
+        }
+
+        this.position = new Position(x, y);
+    }
+
+    draw(canvas) {
+        throw new Error("Draw is not implemented");
+    }
+}
+
+class Circle extends AbstractPhysicsObject {
+    constructor(x, y, diameter) {
+        super(x, y);
+        this.diameter = diameter;
+    }
+
+    draw(canvas) {
+        canvas.fill(65, 223, 250);
+        canvas.circle(this.position.x, this.position.y, this.diameter);
+    }
+}
+
+class World {
+    constructor() {
+        this.objects = new Set();
+    }
+
+    draw(canvas) {
+        canvas.background(247, 245, 246);
+
+        this.objects.forEach((physObj) => {
+            physObj.draw(canvas);
+        })
+    }
+
+    add(physObj) {
+        this.objects.add(physObj);
+    }
+
+    remove(physObj) {
+        this.objects.remove(physObj);
+    }
+}
+
+let simulation = new p5((p) => {
+    p.world = new World();
+
+    p.setup = () => {
+        p.frameRate(60);
+        p.createCanvas(p.windowWidth, p.windowHeight);
+    };
+
+    p.draw = () => {
+        p.world.draw(p);
+    };
+
+    p.mouseClicked = () => {
+        p.world.add(new Circle(p.random(0, p.windowWidth), p.random(0, p.windowHeight), 35));
+    };
+});
