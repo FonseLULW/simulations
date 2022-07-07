58148c017d7e6677ba659204827641a8add4f50d
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-07-06 17:02:36 -0700

    Encapsulated public members into private

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -55,8 +55,8 @@ class Acceleration {
     #y;
 
     constructor(aX, aY) {
-        this.x = aX;
-        this.y = aY;
+        this.#x = aX;
+        this.#y = aY;
     }
 
     get x() {
@@ -93,9 +93,11 @@ class AbstractPhysicsObject {
 }
 
 class Circle extends AbstractPhysicsObject {
+    #diameter;
+
     constructor(x, y, velX, velY, aX, aY, diameter) {
         super(x, y, velX, velY, aX, aY);
-        this.diameter = diameter;
+        this.#diameter = diameter;
 
         console.log(`Created circle at (${this.position.x}, ${this.position.y})
         with v0 = (${this.velocity.x}, ${this.velocity.y})
@@ -104,15 +106,19 @@ class Circle extends AbstractPhysicsObject {
 
     draw(canvas) {
         canvas.fill(65, 223, 250);
-        canvas.circle(this.position.x, this.position.y, this.diameter);
+        canvas.circle(this.position.x, this.position.y, this.#diameter);
 
+        this.update(canvas.deltaTime)
+    }
+
+    update(deltaTimeMS) {
         // px/s = px/s + px/s^2 * ms / 1000
-        this.velocity.x = this.velocity.x + this.acceleration.x * canvas.deltaTime / 1000;
-        this.velocity.y = this.velocity.y + this.acceleration.y * canvas.deltaTime / 1000;
+        this.velocity.x = this.velocity.x + this.acceleration.x * deltaTimeMS / 1000;
+        this.velocity.y = this.velocity.y + this.acceleration.y * deltaTimeMS / 1000;
 
         // m = mp + px/s * ms / 1000
-        this.position.x = this.position.x + this.velocity.x * canvas.deltaTime / 1000;
-        this.position.y = this.position.y + this.velocity.y * canvas.deltaTime / 1000;
+        this.position.x = this.position.x + this.velocity.x * deltaTimeMS / 1000;
+        this.position.y = this.position.y + this.velocity.y * deltaTimeMS / 1000;
     }
 }
 
@@ -157,6 +163,6 @@ let simulation = new p5((p) => {
     };
 
     p.mouseClicked = (e) => {
-        p.world.add(new Circle(e.clientX, e.clientY, 500, -700, 0, 415, 50));
+        p.world.add(new Circle(e.clientX, e.clientY, 500, -700, 0, 415, 50.4));
     };
 });
