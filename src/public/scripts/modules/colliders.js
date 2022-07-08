714b7c97f93df9712f2ddccfb3129ca1c493fc54
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-07-08 15:04:46 -0700

    Added getters and setters to collider.js classes

--- src/public/scripts/modules/colliders.js
+++ src/public/scripts/modules/colliders.js
@@ -1,11 +1,35 @@
 class Collider {
     #origin;
+
+    get x() {
+        return this.#origin.x;
+    }
+
+    set x(x) {
+        this.#origin.x = x;
+    }
+
+    get y() {
+        return this.#origin.y;
+    }
+
+    set y(y) {
+        this.#origin.y = y;
+    }
 }
 
 class CircleCollider extends Collider {
     #diameter;
+
+    get diameter() {
+        return this.#diameter;
+    }
 }
 
 class SquareCollider extends Collider {
     #side;
+
+    get side() {
+        return this.#side;
+    }
 }
